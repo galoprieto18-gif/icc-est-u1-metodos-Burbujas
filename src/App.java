c9fc6a813957bb67296357aef17304e9f4acd565
Galito-18 <238720115+galoprieto18-gif@users.noreply.github.com>
2026-04-20 17:27:58 -0500

    Ordenamientos

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+     Run/Debug
+     ejemplo//prueba de ordenamiento burbuja
+        public static void main(String[] args) throws Exception {
+            int [] numeros = {-5,10,2,0,7};
+            runBubbleSort();
+    }
+
+    public static void runBubbleSort(){
+        System.out.println("Metodo Burbuja");
+        int [] numeros = {-5,10,2,0,7};
+        //Instancia de la clase;
+        // BubbleSort bubbleSort  = new BubbleSort();
+        // bubbleSort.printArreglo(numeros);
+        // bubbleSort.sortAscendente(numeros);
+        // bubbleSort.sortDescendente(numeros);
+        // bubbleSort.printArreglo(numeros);
+        bubbleSort.printArreglo(numeros);
+        bubbleSort.sort(numeros,asc: true);
+        bubbleSort.printArreglo(numeros);
+        bubbleSort.sort(numeros,asc: false);
+        bubbleSort.printArreglo(numeros);
+
+    }
+}
