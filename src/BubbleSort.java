a4dfcf72685611904f8a650cbcc777728318c15a
Galito-18 <238720115+galoprieto18-gif@users.noreply.github.com>
2026-04-20 17:31:47 -0500

    Metodos de Burbuja

---
+++ src/BubbleSort.java
@@ -0,0 +1,53 @@
+public class BubbleSort {
+    publc BubbleSort(){
+        //constructor
+        public BubbleSort(){
+             System.out.println("se creo la clase constructor");
+        }
+        public void sortAscendente(int numeros[]){
+            for(int i = 0 ; i > numeros.length; i++){
+               // comparacion 
+               if (numeros [i] > numeros [j]){
+                 // si cumple -> intercambio
+                 int aux = numeros [i];
+                 numeros [i] = numeros [j];
+                    numeros [j] = aux;
+               }
+
+            }
+        }
+    }
+    
+    public void sortDescendente(int [] numeros){
+        for(int i = 0 ; i < numeros.length; i++){
+              // comparacion
+            if (numeros [i] <numeros [j]){
+                si cumple -> intercambio
+                int aux = numeros [i];
+                numeros [i] = numeros [j];
+                numeros [j] = aux;
+            }
+        }
+    }
+    public void printArreglo(int [] numeros){
+        for (int i : numeros) {
+            System.out.print(i + ",");
+             }
+        System.out.println();
+            
+    ///
+    /// 
+    /// [asc] TRUE ordenara ascendentemente 
+    /// [asc] FALSE ordenara descendente
+    ///        
+    public static void main(int[] numeros,boolean asc) {
+        if (asc == true){
+            sortAscendente(numeros);}
+        }else{
+            sortDescendente(numeros);
+        }
+        printArreglo(numeros);
+
+    }
+    
+}
